f4a1618fc89b23b554e5ea41fbc8e005ef9f5b86
tomasrobino <tomaserobino@gmail.com>
2024-05-27 13:24:55 +0200

    added randomSingle function

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ const board = new Array(HEIGHT).fill(new Array(WIDTH).fill(0));
 function randomCoords(heightMax, widthMax, heightMin = 0, widthMin = 0) {
     return new Array(Math.floor( Math.random() * (heightMax-heightMin) + heightMin ), Math.floor( Math.random() * (widthMax-widthMin) + widthMin ));
 }
-let root = randomCoords(HEIGHT, WIDTH); //Root for DLA
 
+//Returns one random integer, bounded by max and min
+function randomSingle(max, min = 0) {
+    return new Array(Math.floor( Math.random() * (max-min) + min ));
+}
+let root = randomCoords(HEIGHT, WIDTH); //Root for DLA
+board[root[0]][root[1]] = 1;
