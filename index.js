c60cc0db76c9a2e97d8f69d8b52297d183c9ff3a
tomasrobino <tomaserobino@gmail.com>
2024-05-29 11:32:51 +0200

    DLA algorithm working!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WIDTH = 10;
 const HEIGHT = 10;
+const AMOUNT = 30;
 
 //Returns two random integers, bounded by max and min
 function randomCoords(heightMax, widthMax, heightMin = 0, widthMin = 0) {
@@ -12,7 +13,7 @@ function randomSingle(max, min = 0) {
 }
 
 function updateAdjacencies(coordY, coordX) {
-    let adjAmount = 0;
+    let adjAmount = -1;
     if (coordY!==0) {
         if (board[coordY-1][coordX] !== 1 && adjacenciesBoard[coordY-1][coordX] !== 1) {
             adjacenciesBoard[coordY-1][coordX] = 1;
@@ -42,10 +43,34 @@ function updateAdjacencies(coordY, coordX) {
     return adjAmount;
 }
 
+function placeAdjacent(adjAmount) {
+    let random = Math.floor( Math.random() * adjAmount );
+    //Iterate adjacencies board, counting until finding the tile selected with a single random number
+    let counter = 0;
+    for (let i = 0; i < adjacenciesBoard.length; i++) {
+        for (let k = 0; k < adjacenciesBoard[i].length; k++) {
+            if (adjacenciesBoard[i][k]===1) {
+                if (counter===random) {
+                    adjacenciesBoard[i][k] = 0;
+                    board[i][k] = 1;
+                    return updateAdjacencies(i, k);
+                }
+                counter++;
+            }
+        }
+    }
+}
+
 const board = Array.from({length:HEIGHT}, () => new Array(WIDTH).fill(0));
-const adjacenciesBoard = Array.from({length:HEIGHT}, () => new Array(WIDTH).fill(0));
+const adjacenciesBoard = Array.from({length:HEIGHT}, () => new Uint8ClampedArray(WIDTH));
 let adjAmount = 0;
 
 const root = randomCoords(HEIGHT, WIDTH); //Root for DLA
 board[root[0]][root[1]] = 1;
 adjAmount+=updateAdjacencies(root[0], root[1]);
+adjAmount++;
+
+for (let i = 1; i < AMOUNT; i++) {
+    adjAmount+=placeAdjacent(adjAmount);
+}
+//console.log(board);
