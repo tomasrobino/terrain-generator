bf47fcf08ecb65b1ed60581c7eb270e3a8e8a88b
tomasrobino <tomaserobino@gmail.com>
2024-06-05 18:42:19 +0200

    added saveToFile(), deleted useless functions

--- index.js
+++ index.js
@@ -2,86 +2,15 @@ const fs = require("node:fs");
 const sharp = require("sharp");
 
 
-const WIDTH = 10;
-const HEIGHT = 10;
+const WIDTH = 20;
+const HEIGHT = 20;
 const PERCENTAGE_FILLED = 0.2;
-const STAGES_AMOUNT = 1;
+const STAGES_AMOUNT = 2;
 
-//Returns two random integers, bounded by max and min
-function randomCoords(heightMax, widthMax, heightMin = 0, widthMin = 0) {
-    return new Array(Math.floor( Math.random() * (heightMax-heightMin) + heightMin ), Math.floor( Math.random() * (widthMax-widthMin) + widthMin ));
-}
 
 //Returns one random integer, bounded by max and min
 function randomSingle(max, min = 0) {
     return Math.floor( Math.random() * (max-min) + min );
-}
-
-//Update adjacenciesBoard surrounding given coordinates
-function updateAdjacencies(coordY, coordX) {
-    let adjAmount = -1;
-    if (coordY!==0) {
-        if (board[coordY-1][coordX] !== 1 && adjacenciesBoard[coordY-1][coordX] !== 1) {
-            adjacenciesBoard[coordY-1][coordX] = 1;
-            adjAmount++;
-        }
-    }
-    if (coordY!==HEIGHT-1) {
-        if (board[coordY+1][coordX] !== 1 && adjacenciesBoard[coordY+1][coordX] !== 1) {
-            adjacenciesBoard[coordY+1][coordX] = 1;
-            adjAmount++;
-        }
-    }
-    if (coordX!==0) {
-        if (board[coordY][coordX-1] !== 1 && adjacenciesBoard[coordY][coordX-1] !== 1) {
-            adjacenciesBoard[coordY][coordX-1] = 1;
-            adjAmount++;
-        }
-    }
-    if (coordX!==WIDTH-1) {
-        if (board[coordY][coordX+1] !== 1 && adjacenciesBoard[coordY][coordX+1] !== 1) {
-            adjacenciesBoard[coordY][coordX+1] = 1;
-            adjAmount++;
-        }
-    }
-
-    
-    return adjAmount;
-}
-
-//Places tile, calls and returns updateAdjacencies()
-function placeAdjacent(adjAmount) {
-    //Iterate adjacencies board, counting until finding the tile selected with a single random number
-    let flag = true;
-    let aux = 0;
-    let prevAux = 0;
-    while (flag) {
-        let random = Math.floor( Math.random() * adjAmount );
-        let counter = 0;
-        for (let i = 0; i < adjacenciesBoard.length; i++) {
-            for (let k = 0; k < adjacenciesBoard[i].length; k++) {
-                if (adjacenciesBoard[i][k]===1) {
-                    //Placing tile
-                    if (counter===random) {
-                        let adjs = 0;
-                        if (i>0 && board[i-1][k]===1) adjs++;
-                        if (i<board.length-1 && board[i+1][k]===1) adjs++;
-                        if (k>0 && board[i][k-1]===1) adjs++;
-                        if (k<board[i].length-1 && board[i][k+1]===1) adjs++;
-                        if (adjs === 1 || aux-prevAux >20) {
-                            adjacenciesBoard[i][k] = 0;
-                            board[i][k] = 1;
-                            prevAux=aux;
-                            return updateAdjacencies(i, k);
-                        }
-                        aux++;
-                    }
-                    counter++;
-                }
-            }
-        }
-    }
-    
 }
 
 //Get and place root of algorithm, used in every resizing
@@ -96,7 +25,7 @@ function placeRoot(currentHeight, currentWidth, root) {
     //Real position of random
     let position = coordY*currentWidth + coordX;
     for (let i = 0; i < root.length; i++) {
-        if (root[i] === 1) {
+        if (root[i] !== 0) {
             if ((i+1)%Math.floor(currentWidth/2) === 0) {
                 offset += Math.floor(currentWidth/2);
             }
@@ -117,10 +46,15 @@ function printBoard(board, currentWidth) {
     }
 }
 
+async function saveToFile(array, width, height, destination) {
+    let img = sharp(array, {raw: { width: width, height: height, channels: 1 }});
+    await img.toFile(destination);
+}
+
+async function main() {
 let board = new Uint8Array(HEIGHT*WIDTH);
 let blurryBoard = new Int16Array(board.length);
 
-//Amount of times grid will be resized
 for (let i = 0; i < STAGES_AMOUNT; i++) {
     const currentHeight = HEIGHT*(2**i);
     const currentWidth = WIDTH*(2**i);
@@ -208,14 +142,12 @@ for (let i = 0; i < STAGES_AMOUNT; i++) {
     //const ndBoard = ndarray(board, [currentWidth, currentHeight]);
     //const writeable = fs.createWriteStream("results/stage"+(i+1)+".gif");
     //savePixels(ndBoard, "gif").pipe(writeable);
-    
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === 1) board[i] = 255;
+    }
+    await saveToFile(board, currentWidth, currentHeight, "results/stage"+(i+1)+".gif")
 }
-
-
+}
+main()
 //For testing results
 //printBoard(board, WIDTH*(2**(STAGES_AMOUNT-1)))
-for (let i = 0; i < board.length; i++) {
-    if (board[i] === 1) board[i] = 255;
-}
-let img = sharp(board, {raw: { width: WIDTH*(2**(STAGES_AMOUNT-1)), height: HEIGHT*(2**(STAGES_AMOUNT-1)), channels: 1 }});
-img.toFile("results/stage1.gif");
