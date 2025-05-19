608d58d145a60699563d7e3b4ddde39c6c3afbef
Tomas Robino <tomaserobino@gmail.com>
2025-05-20 01:09:19 +0200

    added tests

---
+++ index.test.ts
@@ -0,0 +1,34 @@
+describe('Board class', () => {
+  const width = 10;
+  const height = 10;
+
+  let board: any;
+
+  beforeEach(() => {
+    board = new Board(width, height);
+  });
+
+  test('should initialize with correct dimensions', () => {
+    expect(board.width).toBe(width);
+    expect(board.height).toBe(height);
+    expect(board.board.length).toBe(width * height);
+    expect(board.elevation.length).toBe(width * height);
+  });
+
+  test('static block counter should increment', () => {
+    const initial = Board.getBlockCounter();
+    Board.addBlockCounter();
+    expect(Board.getBlockCounter()).toBe(initial + 1);
+  });
+
+  test('static block counter can be incremented by value', () => {
+    const initial = Board.getBlockCounter();
+    Board.setBlockCounter(5);
+    expect(Board.getBlockCounter()).toBe(initial + 5);
+  });
+
+  test('should throw if width or height are not positive', () => {
+    expect(() => new Board(0, 10)).toThrow();
+    expect(() => new Board(10, 0)).toThrow();
+  });
+});
